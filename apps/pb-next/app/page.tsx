b819afc5f9a55a1160dd3aeabc1300af3f8eba94
arpadsimon <simonarpi89@gmail.com>
2024-03-05 15:46:18 +0100

    remove button from home

--- apps/pb-next/app/page.tsx
+++ apps/pb-next/app/page.tsx
@@ -1,12 +1,7 @@
-import { Button } from '@pb-test/shared';
-
 export default async function Page() {
   return (
     <div className="bg-slate-600 h-screen w-full flex flex-col items-center justify-center gap-6">
       <h1 className="text-white text-6xl">PB POC</h1>
-      <div>
-        <Button>Shared button</Button>
-      </div>
     </div>
   );
 }
